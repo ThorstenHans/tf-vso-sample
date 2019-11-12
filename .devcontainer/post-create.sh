5bdd860f068031476e6f48d328a478d1680c1ce3
Thorsten Hans <thorsten.hans@gmail.com>
2019-11-12 19:05:56 +0100

    crap

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,7 +4,6 @@ sudo apt-get update
 sudo apt-get install wget unzip -y
 cd /home/vsonline
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-zsh
 wget https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip
 unzip terraform_0.12.13_linux_amd64.zip
 sudo ln -s /home/vsonline/terraform /usr/local/bin/terraform
